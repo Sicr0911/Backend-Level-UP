6811619e11d1aee1a80e5043a8f727e7ce683f2b
valparaiso <seb.catalanr@duocuc.cl>
2025-11-12 12:34:39 -0300

    init comit

---
+++ src/main/java/com/example/Backend_Level_UP/BackendLevelUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Level_UP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLevelUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLevelUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Backend_Level_UP/BackendLevelUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_Level_UP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLevelUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
